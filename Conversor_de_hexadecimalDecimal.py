aef99a3d9b93980cc8675dd4b6cd6d353c300cbb
Jhonatan Ferreira <devjhow2003@gmail.com>
2024-07-06 22:09:59 -0300

    first commit

---
+++ Conversor_de_hexadecimalDecimal.py
@@ -0,0 +1,4 @@
+numero_hexadecimal = input('Digite um número hexadecimal para saber seu número decimal correspondente: ')
+
+numero_decimal = int(numero_hexadecimal, 16)
+print(f"O número hexadecimal {numero_hexadecimal} em decimal é {numero_decimal}")
